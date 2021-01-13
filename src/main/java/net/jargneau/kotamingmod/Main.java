8f6125d2d6872e9114b36d135dadafb764538062
Jargneau <34792678+JarnoEetSoep@users.noreply.github.com>
2021-01-13 15:25:44 +0100

    Workspace setup and basic mod

---
+++ src/main/java/net/jargneau/kotamingmod/Main.java
@@ -0,0 +1,12 @@
+package net.jargneau.kotamingmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+    public static final String MOD_ID = "kotaming";
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
